7696665cc61bf6583250da5610e7f92aa4ecb7c7
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-21 18:40:27 +0530

    Testing #8

--- myApp.js
+++ myApp.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 console.log("Hello from myApp");
 
-app.get('/api', (req, res) => {
+app.get('/api', function (req, res) {
     console.log("You have reached the 'api' page!");
     res.json({
         "default": "Default message"
     });
 });
 
-app.get('/api/:date?', (req, res) => {
+app.get('/api/:date?',function (req, res) {
     let normalDate = new Date(req.query);
     res.json({
         "test": normalDate
