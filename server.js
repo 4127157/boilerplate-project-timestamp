9348c7c0ee1048f9e3a0deaf6e76d35e3544a6dd
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-21 21:21:16 +0530

    testing

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/api/hello", function (req, res) {
 app.get('/api/:date?', function (req, res) {
     let test = req.params.date;
     console.log(test);
-    console.log(new Date(test));
+    console.log(new Date(test).toDateString());
     console.log("You have reached the 'api/<something>' page!");
     res.json({
         "default": "Default message"
