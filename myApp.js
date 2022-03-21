82b95c5123e30a622e806a242c67b0d82aa71eab
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-21 17:34:47 +0530

    Testing the api dir

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 console.log("Hello from myApp");
+
+app.get('/api', (req, res) => {
+    console.log("You have reached the 'api' page!");
+});
 app.get('/api/:date?', (req, res) => {
     let normalDate = new Date(req.query);
     res.json({
