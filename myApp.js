841ac57b6801967c1f12ecf0f1c9c76b49c193dd
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-21 18:09:01 +0530

    Testing #6

--- myApp.js
+++ myApp.js
@@ -2,13 +2,9 @@ var express = require('express');
 var app = express();
 console.log("Hello from myApp");
 
-app.use((req, res, next) => {
-    res.json({
-        "test message":"You're seeing this message for no reason"
-    });
-    next();
+app.get('/', (req, res) => {
+    res.sendfile = __dirname + '/views/index.html';
 });
-
 app.get('/api', (req, res) => {
     console.log("You have reached the 'api' page!");
     res.json({
--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// // your first API endpoint... 
+// app.get("/api/hello", function (req, res) {
+//   res.json({greeting: 'hello API'});
+// });
 
 
 
