ceec8a45da209d5f720aff1a525532b7e87f833f
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-21 17:55:47 +0530

    Testing #5

--- myApp.js
+++ myApp.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 console.log("Hello from myApp");
 
+app.use((req, res, next) => {
+    res.json({
+        "test message":"You're seeing this message for no reason"
+    });
+    next();
+});
+
 app.get('/api', (req, res) => {
     console.log("You have reached the 'api' page!");
     res.json({
