cd85dd5197e941a384c095c9a80fa1e753a18fa3
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-21 17:30:27 +0530

    Testing deployment

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+console.log("Hello from myApp");
 app.get('/api/:date?/', (req, res) => {
     let normalDate = new Date(req.query);
     res.json({
