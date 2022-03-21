22b3f27f3eaeca0084ce2536ed77b16502538fee
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-21 17:46:13 +0530

    Testing #4

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ console.log("Hello from myApp");
 
 app.get('/api', (req, res) => {
     console.log("You have reached the 'api' page!");
+    res.json({
+        "default": "Default message
+    });
 });
 
 app.get('/api/:date?', (req, res) => {
